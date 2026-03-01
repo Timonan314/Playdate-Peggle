c3a7c3841dd27637f240b3db000205c94f41141d
Tim <timn783@gmail.com>
2026-03-01 14:59:35 -0500

    I replaced the parabola thing with a circle which will rotate based on the crank pos
    this was really annoying to do because math

--- source/main.lua
+++ source/main.lua
@@ -6,30 +6,33 @@ local pd = playdate
 -- parabola renderer stuff
 local quadxstrech = 0
 
+-- ball output stuff
+local sourceX = 0
+local sourceY = 0
+
 function playdate.update()
     gfx.clear()
     gfx.drawCircleAtPoint(200,30,20)
+
+
+
     local quadCount = 1
     local quadx = 200
     local quady = 30
-    gfx.drawText(tostring(quadxstrech),5,5)
-    while quadCount < 50 do
-        quadxstrech = (pd.getCrankPosition()-180)
-        if quadxstrech == 0 then
-            gfx.drawLine(quadx,quady,quadx,quady+(240-30))
-            quadCount += 1
-        elseif quadxstrech > 0 then
-            gfx.drawLine(quadx, quady, quadx + quadCount, (quadCount^2)/quadxstrech + quady)
-            quadx = quadx + quadCount
-            quady = (quadCount^2)/quadxstrech + quady
-            quadCount += 1
-        elseif quadxstrech < 0 then
-            gfx.drawLine(quadx, quady, quadx - quadCount, -((quadCount^2)/quadxstrech) + quady)
-            quadx = quadx - quadCount
-            quady = -((quadCount^2)/quadxstrech) + quady
-            quadCount += 1
-        end
-    end
+
+    -- to add a circle where the ball will come from, based on the crank position, and a distance from a centre point
+
+    local centerX = 200
+    local centerY = 30
+
+    local sourceDist = 30
+
+    sourceY = centerY-(sourceDist*math.sin(math.rad(90 - pd.getCrankPosition())))
+
+    sourceX = centerX+(sourceDist*math.cos(math.rad(90 - pd.getCrankPosition())))
+
+    gfx.fillCircleAtPoint(sourceX, sourceY, 5)
+
 end
 
 function pd.rightButtonDown()
