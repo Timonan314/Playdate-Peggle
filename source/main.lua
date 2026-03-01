f738b342dab5dccd3d4104b242e87326f43a2cb9
Tim <timn783@gmail.com>
2026-03-01 15:32:23 -0500

    Made the actual source only show  when in the bottom half, and made a ui one that shows no matter what

--- source/main.lua
+++ source/main.lua
@@ -7,7 +7,8 @@ local pd = playdate
 local quadxstrech = 0
 
 -- ball output stuff
-local sourceX = 0
+local sourceDist = 30
+local sourceX = 200 + sourceDist
 local sourceY = 0
 
 function playdate.update()
@@ -25,13 +26,17 @@ function playdate.update()
     local centerX = 200
     local centerY = 30
 
-    local sourceDist = 30
+    if (pd.getCrankPosition()) >= 90 and pd.getCrankPosition() <= 270 then
+        sourceY = centerY-(sourceDist*math.sin(math.rad(90 - pd.getCrankPosition())))
+        sourceX = centerX+(sourceDist*math.cos(math.rad(90 - pd.getCrankPosition())))
+        gfx.fillCircleAtPoint(sourceX, sourceY, 5)
+    else
+        gfx.fillCircleAtPoint(sourceX, centerY, 5)
+        local sourceImagY = centerY-(sourceDist*math.sin(math.rad(90 - pd.getCrankPosition())))
+        local sourceImagX = centerX+(sourceDist*math.cos(math.rad(90 - pd.getCrankPosition())))
+        gfx.drawCircleAtPoint(sourceImagX, sourceImagY, 5)
+    end
 
-    sourceY = centerY-(sourceDist*math.sin(math.rad(90 - pd.getCrankPosition())))
-
-    sourceX = centerX+(sourceDist*math.cos(math.rad(90 - pd.getCrankPosition())))
-
-    gfx.fillCircleAtPoint(sourceX, sourceY, 5)
 
 end
 
