e37af2aa2b9fbd8fe9ccb528fd68c40bb22dd8b5
Tim <timn783@gmail.com>
2026-03-02 13:07:35 -0500

    I added the ball guide parabola thing :)

--- source/main.lua
+++ source/main.lua
@@ -1,16 +1,20 @@
 --editor things
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
 local gfx = playdate.graphics
 local pd = playdate
 
--- parabola renderer stuff
-local quadxstrech = 0
+-- General
+local gameState = "aim"
+local ballSpawned = false
 
 -- ball output stuff
-local sourceDist = 30
-local sourceX = 200 + sourceDist
-local sourceY = 0
-
+local outputDist = 30
+local outputX = 200 + outputDist
+local outputY = 0
+local outputYoffset = -30
+local outputXoffset = 0
+local gravity = 9.8
 function playdate.update()
     gfx.clear()
     gfx.drawCircleAtPoint(200,30,20)
@@ -25,26 +29,40 @@ function playdate.update()
 
     local centerX = 200
     local centerY = 30
-
-    if (pd.getCrankPosition()) >= 90 and pd.getCrankPosition() <= 270 then
-        sourceY = centerY-(sourceDist*math.sin(math.rad(90 - pd.getCrankPosition())))
-        sourceX = centerX+(sourceDist*math.cos(math.rad(90 - pd.getCrankPosition())))
-        gfx.fillCircleAtPoint(sourceX, sourceY, 5)
+    if (pd.getCrankPosition()) >= 80 and pd.getCrankPosition() <= 280 then
+        outputYoffset = -(outputDist*math.sin(math.rad(90 - pd.getCrankPosition())))
+        outputXoffset = (outputDist*math.cos(math.rad(90 - pd.getCrankPosition())))
+        outputY = centerY+outputYoffset
+        outputX = centerX+outputXoffset
     else
-        gfx.fillCircleAtPoint(sourceX, centerY, 5)
-        local sourceImagY = centerY-(sourceDist*math.sin(math.rad(90 - pd.getCrankPosition())))
-        local sourceImagX = centerX+(sourceDist*math.cos(math.rad(90 - pd.getCrankPosition())))
-        gfx.drawCircleAtPoint(sourceImagX, sourceImagY, 5)
+        outputYoffset = -(outputDist*math.sin(math.rad(90 - 280)))
+        outputY = centerY+outputYoffset
+        if (pd.getCrankPosition()) >= 280 then
+            outputXoffset = (outputDist*math.cos(math.rad(90 - 280)))
+            outputX = centerX+outputXoffset
+        else
+            outputXoffset = (outputDist*math.cos(math.rad(90 - 80)))
+            outputX = centerX+outputXoffset
+        end
+        local outputImagYoffset = -(outputDist*math.sin(math.rad(90 - pd.getCrankPosition())))
+        local outputImagXoffset = (outputDist*math.cos(math.rad(90 - pd.getCrankPosition())))
+        local outputImagY = centerY+outputImagYoffset
+        local outputImagX = centerX+outputImagXoffset
+        gfx.drawCircleAtPoint(outputImagX, outputImagY, 5)
+    end
+    gfx.fillCircleAtPoint(outputX, outputY, 5)
+
+    local segments = 0
+    while segments < 50 do
+        gfx.drawLine(outputX,outputY, outputX+outputXoffset/2, outputY+outputYoffset/2)
+        outputX += outputXoffset/2
+        outputY += outputYoffset/2
+        outputYoffset += gravity/2
+        segments += 1
     end
 
-
-end
-
-function pd.rightButtonDown()
-    quadxstrech += 1
-end
-
-
-function pd.leftButtonDown()
-    quadxstrech -= 1
+    if playdate.buttonIsPressed(pd.kButtonB) and gameState == "aim" then
+        gameState = "shoot"
+        ballSpawned = true
+    end
 end
